312d1b015e13d6622f3308d95aded4858e1ae24d
Erwin Julius <erwinjulius@erwinjulius.com>
2013-12-11 15:05:11 -0200

    Flot chart rendering sample

--- standalone_with_tools/src/app/indicadores/controller.js
+++ standalone_with_tools/src/app/indicadores/controller.js
@@ -11,11 +11,25 @@ function($scope) {
 
     $scope.pageName = 'Indicadores de Performance';
 
-    var data = [];
+    
+
+    $scope.mockData = function() {
+        var data = [];
         var series = 5;
         for( var i = 0; i<series; i++) {
           data[i] = { label: "Series"+(i+1), data: Math.floor(Math.random()*100)+1 };
         }
-    $scope.chart_data=data;
+        $scope.chart_data=data;
+        //return data;
+      };
+
+    //inicializa array mock
+    $scope.mockData();
+
+    $scope.user = {
+      dateOfBirth: new Date(1970, 0, 1)
+   }
+
+    console.log('shared/components/flot-chart/controller/chart_data:'+$scope.chart_data);
 
 }]);
--- standalone_with_tools/src/shared/components/flot-chart/directive.flot.chart.js
+++ standalone_with_tools/src/shared/components/flot-chart/directive.flot.chart.js
@@ -10,41 +10,100 @@ function($parse){
     restrict: 'E',
     replace: true,
     transclude: false,
-    compile: function (element, attrs) {
-      var modelAccessor = $parse(attrs.ngModel);
+    //scope:{test:'@'},
+    require: 'ngModel',
+    template:'<div id="{{id}}" style="height: 250px; padding: 0px; position: relative;"></div>',
+    link:function(scope, element, attrs, ngModel) {
+      scope.$watch(function(){
+                    return ngModel.$modelValue;
+                },function(modelValue){
+                    console.log("teste"+modelValue);
+                    jQuery.plot(element, modelValue, {
+                       colors: ['#b9d6fd','#fdb5b5','#c9fdb5','#f9b5fd','#d7b5fd'],       
+                           series: {
+                             pie: { show: true, radius: 1, label: { 
+                                                                    show: true, 
+                                                                    radius: 2/3, 
+                                                                    formatter: function(label, series){return '<div class="pie" tooltip="'+label+'">'+Math.round(series.percent)+'%</div>';},
+                                                                    threshold: 0,
+                                                                    background: { opacity: 0 }
+                                                                  },
+                                                            combine: {
+                                                                        color: '#999',
+                                                                        threshold: 0.05,
+                                                                        label:'outros <5%'
+                                                                      }
+                              }
+                          },
+                          legend: { show: true  }
+                       });
+                });
 
-      var html = "<div type='text' id='" + attrs.id + "' style='height: 250px; padding: 0px; position: relative;'> teste" +
-      "</div>";
+      element.bind("click", function(){
+                    console.log("Teste");
+                    //scope.user.dateOfBirth = new Date();
+                    //regera os dados mock para o chart
+                    scope.mockData();
+                })
 
-      var newElem = $(html);
-      element.replaceWith(newElem);
+      //if(!scope.id) scope.id = attrs.id || '';
 
-      return function (scope, element, attrs, controller) {
-        element.name="teste1";
-      //var processChange = function () {
-      //   var date = new Date(element.datepicker("getDate"));
+      //console.log('shared/components/flot-chart/directive.flot.chart/test:'+scope.test);
+      console.log('shared/components/flot-chart/directive.flot.chart/ngModel: '+ngModel);
+      //element
 
-      //   scope.$apply(function (scope) {
-      //      // Change bound variable
-      //      modelAccessor.assign(scope, date);
-      //   });
-      //};
-
-      //element.datepicker({
-      //   inline: true,
-      //   onClose: processChange,
-      //   onSelect: processChange
-      //});
-
-      scope.$watch(modelAccessor, function (val) {
-         var name = val;
-         element.name="teste1";
-      });
-
-      };
 
     }
+    // compile: function (element, attrs) {
+
+
+    //   var modelAccessor = $parse(attrs.ngModel);
+    //   console.log('shared/components/flot-chart/directive.flot.chart/ngModel:'+attrs.ngModel);
+      
+
+    //   var html = "<div type='text' id='" + attrs.id + "' style='height: 250px; padding: 0px; position: relative;'> teste" +
+    //   "</div>";
+
+    //   var newElem = $(html);
+    //   element.replaceWith(newElem);
+
+    //   return function (scope, element, attrs, controller) {
+    //     element.id="teste1";
+
+    //     var data=[];
+        
+        
+    //     modelAccessor.assign(scope, data);
+        
+        
+
+    //     console.log('shared/components/flot-chart/directive.flot.chart/modelAccessor:'+data);
+
+    //   //var processChange = function () {
+    //   //   var date = new Date(element.datepicker("getDate"));
+
+    //   //   scope.$apply(function (scope) {
+    //   //      // Change bound variable
+    //   //      modelAccessor.assign(scope, date);
+    //   //   });
+    //   //};
+
+    //   //element.datepicker({
+    //   //   inline: true,
+    //   //   onClose: processChange,
+    //   //   onSelect: processChange
+    //   //});
+
+    //   scope.$watch(modelAccessor, function (val) {
+    //      var chart_data = val;
+    //      
+    //   });
+
+    //   };
+
+    // }
 
   };
 
 });
+                              
